fb592f4443c1f858567fba345feb878e9d852484
frisbee09 <frisbee09@users.noreply.github.com>
2021-05-05 20:59:45 +0100

    Aaaaand go

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { CVGrid } from "./structureSC";
+
+interface IAppProps {}
+
+const App: React.FunctionComponent<IAppProps> = (props) => {
+  return <CVGrid>Hi</CVGrid>;
+};
+
+export default App;
---
+++ src/components/structureSC.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const CVGrid = styled.div`
+    display: grid;
+    max-width: 800px;
+`;
