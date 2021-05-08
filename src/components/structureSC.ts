a42aa24138a34c6482d910d64aedfa56a2c1676a
frisbee09 <frisbee09@users.noreply.github.com>
2021-05-08 17:05:16 +0100

    Some layout work

--- src/components/structureSC.ts
+++ src/components/structureSC.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
 export const CVGrid = styled.div`
+	margin: auto;
 	display: grid;
-	max-width: 800px;
-	background: blue;
+	max-width: 1000px;
+	height: 100%;
+	background: ${props => props.theme.primary.getHexA()};
 `;
